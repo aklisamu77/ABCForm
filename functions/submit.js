f8a44b5ebe2e773daf6651dea192eae26720cddc
islam khamis <islam.ahmed@proleaderseg.com>
2023-10-17 20:26:04 +0300

    add test

--- functions/submit.js
+++ functions/submit.js
@@ -3,9 +3,8 @@
  */
 export async function onRequestPost(context) {
   try {
+    //
 
-    // 
-    
     let input = await context.request.formData();
 
     // Convert FormData to JSON
@@ -19,40 +18,77 @@ export async function onRequestPost(context) {
       }
     }
 
-
     // validate honeypot
     if (output["honeypot"] !== "") {
-       
-      return new Response(`Error: The honeypot field must be empty.`, { status: 400 });
+      return new Response(`Error: The honeypot field must be empty.`, {
+        status: 400,
+      });
     }
 
-    // validate email 
+    // validate email
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     if (!emailRegex.test(output["email"])) {
       return new Response(`Error: Email not correct.`, { status: 400 });
     }
-    
+
+    // check spam
+    const apiKey = "qFYe6srB3AdnztutDeQPCmhe9w4k3kMlKAfOYs0G";
+    ip = await getIPAddress();
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        content: output["name"],
+        ip: ip,
+      }),
+    };
+
+    fetch("https://api.oopspam.com/v1/spamdetection", requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+         
+        alert(data.score); 
+        console.log("Score:", data.score);
+        console.log("Details:", data.details);
+      })
+      .catch((error) => {
+        throw new Error('Error');
+      });
 
     const options = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': context.env.FORM_SERVICE_API_KEY
-        },
-        body: JSON.stringify(output),
-      };
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: context.env.FORM_SERVICE_API_KEY,
+      },
+      body: JSON.stringify(output),
+    };
 
-    const result = await fetch('https://contactform.cleaner-web.com/', options)
+    const result = await fetch("https://contactform.cleaner-web.com/", options);
 
     if (result.status == 201) {
-        return Response.redirect('https://cleaner-web.com/form-success/', 301);
-      } else {
-      return new Response(JSON.stringify({ message: 'Message submission failed!', result }), {
-        headers: { 'content-type': 'text/json' },
-        status: 400,
-      });
-    };
+      return Response.redirect("https://cleaner-web.com/form-success/", 301);
+    } else {
+      return new Response(
+        JSON.stringify({ message: "Message submission failed!", result }),
+        {
+          headers: { "content-type": "text/json" },
+          status: 400,
+        }
+      );
+    }
   } catch (err) {
     return new Response(`Error: ${err}`, { status: 400 });
   }
+}
+
+
+async function getIPAddress() {
+  const response = await fetch('https://api.ipify.org?format=json');
+  const data = await response.json();
+  return data.ip;
 }
